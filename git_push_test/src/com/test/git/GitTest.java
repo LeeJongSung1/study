edff1d01012fa830ee4c45fe77e0a4681c0e0127
BIG-01 <BIG-01@BIG-01-PC>
2017-12-07 16:29:45 +0900

    modify GitTest.java : println("와우~~....

--- git_push_test/src/com/test/git/GitTest.java
+++ git_push_test/src/com/test/git/GitTest.java
@@ -3,7 +3,7 @@ package com.test.git;
 public class GitTest {
 	public static void main(String[] args) {
 		
-		System.out.println("와우~~~ 내일은 삼겹살~");
+		System.out.println("와우~~내일은 시험 보는날~~!!");
 		
 	}
 }
